8729554fa78412d484e320333cca51e033a5de5d
Ahmad Mario <riotheassassin@gmail.com>
2024-10-28 16:57:44 +0700

    Adding header, footer, and home page

--- footer.php
+++ footer.php
@@ -1,4 +1,24 @@
-    <footer class="bd-footer py-2 bg-body-tertiary absolute-bottom" style="font-family: Montserrat;">
+</main>
+    <footer class="bd-footer py-2 bg-body-tertiary absolute-bottom" style="font-family: Montserrat; background-color: #123411; text-decoration: none; color:#F2E9DC;">
+        <div class="container">
+            <div class="row">
+                <div class="col flex-column">
+                    <a class="nav-link" href="index.php?p=home">Beranda</a>
+                    <a class="nav-link" href="index.php?p=catalogue">Katalog</a>
+                    <a class="nav-link" href="index.php?p=about">Tentang Kami</a>
+                </div>
+                <div class="col flex-column">
+                    <p>0857-3592-8445</p>
+                    <p>natureglass123@gmail.com</p>
+                    <p>@natureglass.idn</p>
+                </div>
+                <div class="col flex-column text-end" style="color:#91C499">
+                    <p>@2024 Nature Glass</p>
+                    <p>Hak Cipta Dilindungi</p>
+                    <p><strong>Bring Nature, Bring Harmony</strong></p>
+                </div>
+            </div>
+        </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
 </body>
--- header.php
+++ header.php
@@ -8,10 +8,15 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
-    <header style="font-family: Montserrat;">
+    <header style="background-color: #91C499;font-family: Montserrat;">
     <nav class="navbar container">
             <a class="navbar-brand" href="index.php?p=home">Nature Glass</a>
+            <div class="d-flex align-middle">
+                <a class="nav-link m-2" href="index.php?p=home">Beranda</a>
+                <a class="nav-link m-2" href="index.php?p=catalogue">Katalog</a>
+                <a class="nav-link m-2" href="index.php?p=about">Tentang Kami</a>
+            </div>
     </nav>        
     </header>
     <br>
-    <main class="container">
+    <main>
