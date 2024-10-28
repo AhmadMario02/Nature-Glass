cac89e3bd112d6d24daa6d9ab3e2b7210ad74f21
Ahmad Mario <riotheassassin@gmail.com>
2024-10-28 14:45:08 +0700

    Add basic pages

---
+++ footer.php
@@ -0,0 +1,5 @@
+    <footer class="bd-footer py-2 bg-body-tertiary absolute-bottom" style="font-family: Montserrat;">
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
+</body>
+</html>
