4ca8783dad572afc1e10c9b89c5449606d29c46e
Ahmad Mario <riotheassassin@gmail.com>
2024-10-30 20:50:01 +0700

    Add catalogue page

---
+++ pages/catalogue.php
@@ -0,0 +1,34 @@
+<div class="container">
+    <h1 class="text-center" style="font-family: Montserrat;">Katalog Produk</h1>
+    <h1 class="text-center mb-5" style="font-family: Montserrat; color:#123411;">Terrarium Package</h1>
+    <div class="row mb-5">
+        <div class="col-4">
+            <img src="assets/Barren Desert Terrarium.jpg" class="img-fluid rounded">
+        </div>
+        <div class="col-8">
+            <h2>Barren Desert</h2>
+            <hr class="border border-1">
+            <p>Deskripsi produk</p>
+        </div>
+    </div>
+    <div class="row mb-5">
+        <div class="col-8 text-end">
+            <h2>Forest</h2>
+            <hr class="border border-1">
+            <p>Deskripsi produk</p>
+        </div>
+        <div class="col-4">
+            <img src="assets/Forest Terrarium.jpg" class="img-fluid rounded">
+        </div>
+    </div>
+    <div class="row mb-5">
+        <div class="col-4">
+            <img src="assets/Dune Terrarium.jpg" class="img-fluid rounded">
+        </div>
+        <div class="col-8">
+            <h2>Dune</h2>
+            <hr class="border border-1">
+            <p>Deskripsi produk</p>
+        </div>
+    </div>
+</div>
