63171b07dc9d3d822f90606f766b7e24598da1d3
Ahmad Mario <riotheassassin@gmail.com>
2024-12-26 06:44:41 +0700

    Change

--- pages/home.php
+++ pages/home.php
@@ -1,50 +1,14 @@
 <div>
-    <div class="row mb-5">
-        <div class="col text-center" style="background-color: #F2E9DC;">
-            <h1 class="fw-bold">Call-To-Action</h1>
-        </div>
-        <div class="col text-center">
-            <img src="https://images.pexels.com/photos/886521/pexels-photo-886521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="img-fluid" alt="">
-        </div>
+  <div class="row mb-5">
+    <div class="col text-center" style="background-color: #F2E9DC;">
+      <h1 class="fw-bold">Call-To-Action</h1>
     </div>
-    <div class="container">
-    <div class="row mb-5">
-        <h1 class="text-center" style="font-family: Montserrat;">Produk Unggulan</h1>
-        <div class="row">
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                  <img src="assets/Barren Desert Terrarium.jpg" class="card-img-top" alt="Barren Desert Terrarium">
-                  <div class="card-body">
-                    <h5 class="card-title">Barren Desert Terrarium</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Lihat detail</a>
-                  </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                  <img src="assets/Forest Terrarium.jpg" class="card-img-top" alt="Forest Terrarium">
-                  <div class="card-body">
-                    <h5 class="card-title">Forest Terrarium</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Lihat detail</a>
-                  </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                  <img src="assets/Dune Terrarium.jpg" class="card-img-top" alt="Dune Terrarium">
-                  <div class="card-body">
-                    <h5 class="card-title">Dune Terrarium</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Lihat detail</a>
-                  </div>
-                </div>
-            </div>
-        </div>
+    <div class="col text-center">
+      <img src="https://images.pexels.com/photos/886521/pexels-photo-886521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="img-fluid" alt="">
     </div>
-    </div>
-    <div class="row" style="background-color: #D9D9D9;">
-        <h1 class="text-center" style="font-family: Montserrat;">Why must Nature Glass?</h1> 
-    </div>
+  </div>
+</div>
+<div class="row" style="background-color: #D9D9D9;">
+  <h1 class="text-center" style="font-family: Montserrat;">Why must Nature Glass?</h1>
+</div>
 </div>
