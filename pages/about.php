6f14330196355092844b219e59486c9aae620d1c
Ahmad Mario <riotheassassin@gmail.com>
2024-12-26 12:45:54 +0700

    page Tentang Kami

---
+++ pages/about.php
@@ -0,0 +1,44 @@
+<section>
+    <div class="container" style="font-family: Poppins;">
+        <h1 class="text-center">Tentang Kami</h1>
+        <div class="row">
+            <div class="col fs-1">
+                <h1>Bring Nature, Bring Harmony</h1>
+                <h1>1 Juta Tanaman Untuk Indonesia</h1>
+            </div>
+            <div class="col">
+                <p>Nature Glass adalah Edukasi Alam dan Empati untuk Generasi Masa Depan Nature Terrarium hadir sebagai pelopor edutainment untuk anak-anak melalui terrarium tematik yang menggabungkan edukasi, kreativitas, dan nilai tambah. Setiap paketnya berisi terrarium bertema (Gurun, Pantai, Hutan dll), kisah edukasi tentang pelestarian alam, dan aktivitas kreatif. Lebih dari sekadar mainan, Nature Terrarium bertujuan menumbuhkan kecintaan alam dan empati pada anak sejak dini. Kami percaya bahwa dengan edukasi dan pengalaman menyenangkan ini, kita dapat melahirkan generasi yang bertanggung jawab dan peduli terhadap lingkungan, mewujudkan Indonesia Emas dengan kelestarian alam dan kesehatan bumi.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section style="height: 200px; background-image: url(https://images.pexels.com/photos/5830772/pexels-photo-5830772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2); background-size:cover; background-position:center; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" class="text-center text-white my-5">
+</section>
+<section class="mb-5">
+    <div class="container" style="font-family: Poppins;">
+        <div class="row mb-3">
+            <div class="col-8">
+                <h2>Visi Kami</h2>
+                <h3>Bring Nature To The World! 1 Juta Tanaman Untuk Indonesia</h3>
+                <p>Menjadi pelopor edutainment terdepan di Indonesia yang menumbuhkan kecintaan alam dan empati pada generasi masa depan, demi mewujudkan Indonesia Emas dengan kelestarian alam dan kesehatan bumi.</p>
+            </div>
+            <div class="col-4">
+                <img src="https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&w=800" class="rounded img-fluid" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <img src="https://images.pexels.com/photos/1072824/pexels-photo-1072824.jpeg?auto=compress&cs=tinysrgb&w=800" class="rounded img-fluid" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+            </div>
+            <div class="col-8">
+                <h2>Misi Kami</h2>
+                <ul>
+                    <li>Meningkatkan kesadaran masyarakat tentang pentingnya menjaga kelestarian alam sejak dini.</li>
+                    <li>Memupuk rasa cinta, empati dan tanggung jawab terhadap alam sejak dini, demi melahirkan generasi yang siap melindungi dan melestarikan lingkungan.</li>
+                    <li>Menyebarkan keindahan tanaman di setiap ruangan.</li>
+                    <li>Menjadi pusat edukasi anak, belajar menanam menjadi menyenangkan.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
